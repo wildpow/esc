7c40667ecd55f60c10c509155658f98540d4a145
wildpow <element1028@gmail.com>
2019-07-01 17:30:09 -0700

    base and new orice drop down for base

---
+++ src/components/imageViewer_NEW.js
@@ -0,0 +1,154 @@
+import React from "react";
+import styled from "styled-components";
+import PropTypes from "prop-types";
+import { NodeGroup } from "react-move";
+import {
+  ImgWrapper,
+  SmallImgHolder,
+  SmImg,
+  LargeImg,
+} from "../styles/imageViewerStyles";
+import { Banner } from "./mattThumbNail/mattThumbStyles";
+
+const BigBanner = styled(Banner)`
+  font-size: 0.6rem;
+  @media (min-width: 360px) and (orientation: portrait) {
+    font-size: 0.7rem;
+    margin-left: 5px;
+    padding-top: 5px;
+    padding-bottom: 5px;
+    padding-left: 10px;
+    width: 75%;
+    /* padding-right: 5px; */
+    /* text-align: left; */
+  }
+
+  @media (max-width: 736px) and (orientation: landscape) {
+    font-size: 0.8rem;
+    margin-left: 0px;
+    width: 80%;
+    /* text-align: left; */
+    padding-top: 5px;
+    padding-bottom: 5px;
+    padding-left: 10px;
+    padding-right: 5px;
+  }
+  @media (min-width: 810px) and (orientation: landscape) {
+    margin-left: 0;
+    width: 75%;
+  }
+  @media (min-width: 768px) and (orientation: portrait) {
+    font-size: 1.1rem;
+    padding-top: 7px;
+    padding-bottom: 7px;
+    padding-left: 10px;
+    padding-right: 0px;
+    width: 75%;
+  }
+  @media (min-width: 1024px) and (orientation: landscape) {
+    font-size: 1.3rem;
+    padding-top: 7px;
+    padding-bottom: 7px;
+    padding-left: 20px;
+    width: 70%;
+    /* text-align: left; */
+  }
+  @media print {
+    font-size: 1.4rem;
+    border-bottom: 4px solid ${props => props.theme.mainColor2};
+    color: black;
+    text-align: left;
+  }
+`;
+class ImageViewer extends React.PureComponent {
+  static propTypes = {
+    cover: PropTypes.string.isRequired,
+    img1: PropTypes.string.isRequired,
+    img2: PropTypes.string.isRequired,
+    fullname: PropTypes.string.isRequired,
+    type: PropTypes.string.isRequired,
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentImageIndex: 0,
+    };
+  }
+
+  changeActiveImage = event => {
+    this.setState({
+      currentImageIndex: Number(event.currentTarget.dataset.id),
+    });
+  };
+
+  render() {
+    const { cover, img1, img2, fullname, type, base, saleBanner } = this.props;
+    const { currentImageIndex } = this.state;
+    const ImagesArray = [cover, img1, img2];
+    return (
+      <ImgWrapper>
+        <NodeGroup
+          data={[currentImageIndex]}
+          keyAccessor={d => d}
+          start={() => ({
+            opacity: 0,
+          })}
+          enter={() => ({
+            opacity: [1],
+            timing: { duration: 300 },
+          })}
+          update={() => ({
+            opacity: [1],
+            timing: { duration: 300 },
+          })}
+          leave={() => ({
+            opacity: [0],
+            timing: { duration: 300 },
+          })}
+        >
+          {nodes => (
+            <div style={{ position: "relative" }}>
+              {nodes.map(({ key, data, state: { opacity } }) => (
+                <div key={key} style={{ opacity, position: "relative" }}>
+                  {data === 0 && saleBanner && (
+                    <BigBanner>{saleBanner}</BigBanner>
+                  )}
+                  <LargeImg
+                    src={ImagesArray[data]}
+                    alt={`Large image of ${fullname} ${type}`}
+                    style={{ position: "absolute" }}
+                  />
+                </div>
+              ))}
+            </div>
+          )}
+        </NodeGroup>
+        <SmallImgHolder style={{ position: "relative" }} base={base}>
+          <SmImg
+            onMouseOver={this.changeActiveImage}
+            onFocus={this.changeActiveImage}
+            src={ImagesArray[0]}
+            data-id={0}
+            alt={`Small image of ${fullname} ${type}`}
+          />
+          <SmImg
+            onMouseOver={this.changeActiveImage}
+            onFocus={this.changeActiveImage}
+            src={ImagesArray[1]}
+            data-id={1}
+            alt={`Small image of ${fullname} ${type}`}
+          />
+          <SmImg
+            onMouseOver={this.changeActiveImage}
+            onFocus={this.changeActiveImage}
+            src={ImagesArray[2]}
+            data-id={2}
+            alt={`Small image of ${fullname} ${type}`}
+          />
+        </SmallImgHolder>
+      </ImgWrapper>
+    );
+  }
+}
+export default ImageViewer;
--- src/templates/base.js
+++ src/templates/base.js
@@ -1,85 +1,243 @@
 import React from "react";
+import AnchorLink from "react-anchor-link-smooth-scroll";
+import { HelmetDatoCms } from "gatsby-source-datocms";
+import styled from "styled-components";
 import PropTypes from "prop-types";
 import { graphql } from "gatsby";
 import Layout from "../components/layout";
 import BreadCrumbs, { BreadWrapper } from "../components/breadCrumbs";
-import ImageViewer from "../components/imageViewer";
-import DropDown from "../components/dropDown";
+import ImageViewer from "../components/imageViewer_NEW";
+// import DropDown from "../components/dropDown_NEW";
+import PriceDropDown from "../components/priceDropDown_NEW";
 import {
   Wrapper,
   Main,
   MainInfo,
-  PriceWrapper,
-  Price,
-  PriceTitle,
+  // PriceWrapper,
+  // Price,
+  // PriceTitle,
   Warranty,
   Description,
   Article,
-  StyledMarkDown,
+  // StyledMarkDown,
   Profile,
   MainTitle,
-  InfoAnchor,
-  Stuff,
-  Construction,
+  // InfoAnchor,
+  // Stuff,
+  // Construction,
 } from "../styles/singleMattStyles";
-import SEO from "../components/seo";
+// import SEO from "../components/seo";
 
+const List = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  font-family: ${props => props.theme.MainFont3};
+  font-weight: 400;
+  margin-left: 5px;
+  color: ${props => props.theme.newColor2};
+  padding: 0px;
+  h3 {
+    font-size: 0.9rem;
+    margin-top: 0;
+    margin-bottom: 0;
+    border-bottom: 4px solid ${props => props.theme.mainColor2};
+    padding-bottom: 2px;
+  }
+  ul {
+    list-style: square;
+    margin-top: 2px;
+    font-size: 0.7rem;
+    padding-left: 20px;
+    margin-bottom: 5px;
+  }
+  ul li {
+    padding-bottom: 2px;
+  }
+
+  @media (min-width: 360px) {
+    & ul {
+      margin-top: 10px;
+    }
+  }
+
+  @media (min-width: 550px) {
+    padding: 0px 0px 0px 10px;
+    h3 {
+      font-size: 1.8rem;
+      margin-top: 0;
+      margin-bottom: 0;
+      border-bottom: 4px solid ${props => props.theme.mainColor2};
+      padding-bottom: 2px;
+    }
+    ul {
+      list-style: square;
+      margin-top: 10px;
+      font-size: 1rem;
+    }
+    ul li {
+      padding-bottom: 2px;
+      font-size: 1.1rem;
+    }
+  }
+
+  @media (min-width: 992px) {
+    padding: 0px 30px 10px 30px;
+
+    h3 {
+      font-size: 2.4rem;
+      margin-top: 0;
+      margin-bottom: 0;
+      padding-bottom: 2px;
+    }
+    ul {
+      margin-top: 10px;
+      font-size: 1.8rem;
+    }
+    ul li {
+      padding-bottom: 2px;
+      font-size: 1.8rem;
+    }
+  }
+
+  @media (min-width: 1300px) {
+    h3 {
+      font-weight: 700;
+      font-size: 2.1rem;
+    }
+  }
+
+  @media print {
+    p {
+      font-size: 1.3rem;
+    }
+    li {
+      font-size: 1rem;
+    }
+  }
+`;
+
+const Construction = styled(List)`
+  @media (min-width: 992px) {
+    h3 {
+      font-size: 1.8rem;
+    }
+    ul {
+      font-size: 1.6rem;
+    }
+    ul li {
+      font-size: 1.4rem;
+    }
+  }
+  @media (min-width: 1300px) {
+    h3 {
+      font-weight: 700;
+      font-size: 1.8rem;
+    }
+  }
+`;
+
+const Info = styled.li`
+  padding-top: 10px;
+  list-style: none;
+  a {
+    display: none;
+    /* margin-left: 5px;
+    padding-left: 15px; */
+    font-size: 0.9rem;
+    font-family: ${props => props.theme.MainFont1};
+    font-weight: 700;
+    letter-spacing: 0.05rem;
+    color: ${props => props.theme.mainColor2};
+    &:hover {
+      color: ${props => props.theme.mainColor1};
+    }
+    @media (orientation: landscape) {
+      display: block;
+    }
+    @media (min-width: 568px) {
+      /* padding-left: 30px; */
+      font-size: 1rem;
+    }
+    @media (min-width: 768px) {
+      display: block;
+      font-size: 1.2rem;
+    }
+    @media (min-width: 1024px) {
+      /* padding-left: 55px; */
+      font-size: 1.6rem;
+    }
+    @media print {
+      display: none;
+    }
+  }
+`;
 const Base = ({ data }) => {
-  const { AdjBases } = data.gcms;
+  const { datoCmsAdjustableBase: adjBase } = data;
   return (
     <Layout>
       <BreadWrapper>
-        <BreadCrumbs next="Adjustable" here={AdjBases.fullName} />
+        <BreadCrumbs next="Adjustable" here={adjBase.fullName} />
       </BreadWrapper>
       <Wrapper>
-        <SEO
-          title={`ESC: ${AdjBases.fullName}`}
-          description={AdjBases.baseDescription}
-          ogURL={`https://www.escmattresscenter.com/adjustable/${AdjBases.uri}`}
-          ogImage={`https://media.graphcms.com/resize=w:1200,h:627,fit:clip/${
-            AdjBases.coverImg.handle
-          }`}
-          ogImageAlt={`E.S.C Mattress Center | ${AdjBases.fullName}`}
-          ogTitle={`E.S.C Mattress Center | ${AdjBases.fullName}`}
-        />
+        <HelmetDatoCms seo={adjBase.seoMetaTags} />
         <header>
-          <MainTitle>{AdjBases.fullName}</MainTitle>
+          <MainTitle>{adjBase.fullName}</MainTitle>
         </header>
         <Main>
           <ImageViewer
-            saleBanner={AdjBases.saleBanner}
-            cover={AdjBases.coverImg.handle}
-            img1={AdjBases.detail1.handle}
-            img2={AdjBases.detail2.handle}
+            saleBanner={adjBase.sale[0].saleBanner}
+            cover={adjBase.images3[0].url}
+            img1={adjBase.images3[1].url}
+            img2={adjBase.images3[2].url}
             type="adjustable base without mattress"
-            fullname={AdjBases.fullName}
+            fullname={adjBase.fullName}
             base
           />
           <MainInfo>
-            <Stuff>
-              <StyledMarkDown source={AdjBases.features} escapeHtml={false} />
-              <InfoAnchor href="#moreInfo">See more details</InfoAnchor>
-            </Stuff>
-            <PriceWrapper>
-              <Price>
-                <PriceTitle>Base Price</PriceTitle>
-                <DropDown data={AdjBases.price} data2={AdjBases.salePrice} />
-              </Price>
-            </PriceWrapper>
+            <List>
+              <h3>Features:</h3>
+              <ul>
+                {adjBase.smallFeatureList.map(item => (
+                  <li key={item.feature}>{item.feature}</li>
+                ))}
+                <Info>
+                  <AnchorLink href="#moreInfo">See more details</AnchorLink>
+                </Info>
+              </ul>
+            </List>
+            {/* <PriceWrapper> */}
+            {/* <Price> */}
+            {/* <PriceTitle>PRICE:</PriceTitle> */}
+            {/* {console.log(adjBase.price[0])}
+                {console.log(adjBase.sale[0])} */}
+            <PriceDropDown
+              price={adjBase.price[0]}
+              discount={adjBase.sale[0].discount}
+            />
+            {/* </Price> */}
+            {/* </PriceWrapper> */}
           </MainInfo>
         </Main>
         <header id="moreInfo">
           <MainTitle red>OVERVIEW & SPECS</MainTitle>
         </header>
         <Article>
-          <Description>{AdjBases.baseDescription}</Description>
-          <Profile>{`Profile: ${AdjBases.height}`}</Profile>
-          <Construction source={AdjBases.keyfeatures} escapeHtml={false} />
-          <Warranty>{AdjBases.warranty}</Warranty>
+          <Description>{adjBase.description}</Description>
+          <Profile>{`Profile: ${adjBase.height}`}</Profile>
+          <Construction>
+            <h3>Key Features:</h3>
+            <ul>
+              {adjBase.fullFeatureList.map(item => (
+                <li key={item.feature}>{item.feature}</li>
+              ))}
+            </ul>
+          </Construction>
+          <Warranty>{adjBase.warranty}</Warranty>
         </Article>
       </Wrapper>
       <BreadWrapper>
-        <BreadCrumbs next="adjustable" here={AdjBases.fullName} />
+        <BreadCrumbs next="adjustable" here={adjBase.fullName} />
       </BreadWrapper>
     </Layout>
   );
@@ -90,31 +248,34 @@ Base.propTypes = {
 export default Base;
 
 export const query = graphql`
-  query SingleAjustableQuery($uri: String!) {
-    gcms {
-      AdjBases(uri: $uri) {
+  query singleAdjustable($uri: String!) {
+    datoCmsAdjustableBase(uri: { eq: $uri }) {
+      fullName
+      description
+      images3 {
+        url
+      }
+      warranty
+      height
+      price {
+        twin
+        twinxl
+        full
+        queen
+        king
+      }
+      sale {
         saleBanner
-        id
-        uri
-        fullName
-        keyfeatures
-        features
-        price
-        salePrice
-        # brandLine
-        # brandName
-        baseDescription
-        height
-        warranty
-        coverImg {
-          handle
-        }
-        detail1 {
-          handle
-        }
-        detail2 {
-          handle
-        }
+        discount
+      }
+      seoMetaTags {
+        ...GatsbyDatoCmsSeoMetaTags
+      }
+      smallFeatureList {
+        feature
+      }
+      fullFeatureList {
+        feature
       }
     }
   }
