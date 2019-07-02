9d2d172a1ec7024676ac8de5289ae66713013818
wildpow <element1028@gmail.com>
2019-07-01 17:19:25 -0700

    Switched generated pages to New CMS

--- gatsby-node.js
+++ gatsby-node.js
@@ -13,113 +13,59 @@ exports.onCreateWebpackConfig = ({ actions, stage }) => {
 exports.createPages = async ({ actions, graphql }) => {
   const { data } = await graphql(`
     query {
-      gcms {
-        allPosts(orderBy: dateAndTime_DESC, filter: { isPublished: true }) {
-          id
-          slug
-          isPublished
-          title
-          dateAndTime
-          content
-          bottomimg {
-            handle
-            width
-            height
-          }
-          coverImage {
-            handle
+      allDatoCmsAdjustableBase(sort: { fields: meta___status, order: DESC }) {
+        edges {
+          node {
+            uri
+            slug
           }
         }
-        allAdjBaseses(filter: { isPublished: true }) {
-          isPublished
-          id
-          brandName
-          warranty
-          brandLine
-          baseDescription
-          uri
-          value
-          height
-          features
-          fullName
-          keyfeatures
-          salePrice
-          price
-          coverImg {
-            height
-            handle
-            width
-          }
-          detail1 {
-            height
-            handle
-            width
-          }
-          detail2 {
-            height
-            handle
-            width
+      }
+      allDatoCmsMattress(sort: { fields: meta___status }) {
+        edges {
+          node {
+            slug
+            uri
+            brand {
+              urlName
+            }
           }
         }
-        allMattresses(filter: { isPublished: true }) {
-          isOnSale {
-            saleName
-          }
-          id
-          freeBoxSpring
-          isPublished
-          uri
-          uriBrandName
-          brandName
-          subName
-          subBrand
-          name
-          discription
-          features
-          profile
-          contruction
-          mattOnly
-          mattOnlySale
-          setPrice
-          setPriceSale
-          warranty
-          coverImg {
-            handle
-          }
-          detail1 {
-            handle
-          }
-          detail2 {
-            handle
+      }
+      allDatoCmsBlog {
+        edges {
+          node {
+            slug
           }
         }
       }
     }
   `);
-  data.gcms.allPosts.forEach(blog => {
+  data.allDatoCmsAdjustableBase.edges.forEach(base => {
+    // const reg = `/(${base.node.slug})/gi`;
     actions.createPage({
-      path: `/blog/${blog.slug}`,
-      component: path.resolve(`./src/templates/post.js`),
-      context: {
-        slug: blog.slug,
-      },
-    });
-  });
-  data.gcms.allAdjBaseses.forEach(base => {
-    actions.createPage({
-      path: `/adjustable/${base.uri}`,
+      path: `/adjustable/${base.node.uri}`,
       component: path.resolve(`./src/templates/base.js`),
       context: {
-        uri: base.uri,
+        uri: base.node.uri,
       },
     });
   });
-  data.gcms.allMattresses.forEach(mattress => {
+  data.allDatoCmsMattress.edges.forEach(mattress => {
     actions.createPage({
-      path: `/brands/${mattress.uriBrandName.toLowerCase()}/${mattress.uri}`,
+      path: `/brands/${mattress.node.brand.urlName}/${mattress.node.uri}`,
       component: path.resolve(`./src/templates/mattress.js`),
       context: {
-        uri: mattress.uri,
+        uri: mattress.node.uri,
+      },
+    });
+  });
+  data.allDatoCmsBlog.edges.forEach(blog => {
+    actions.createPage({
+      path: `/blog/${blog.node.slug}`,
+      component: path.resolve(`./src/templates/post.js`),
+      context: {
+        slug: blog.node.slug,
       },
     });
   });
