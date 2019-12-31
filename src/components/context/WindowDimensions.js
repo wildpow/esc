6b001d92e21f2c18c19d9bb38ad62bbd7cb9fa19
wildpow <element1028@gmail.com>
2019-12-31 14:51:11 -0800

    updated windowDimensions context
    now window dimensions context throttles instead of firing on every resize.

--- src/components/context/WindowDimensions.js
+++ src/components/context/WindowDimensions.js
@@ -1,24 +1,43 @@
-import React, { createContext, useContext, useState, useEffect } from "react";
+import React, {
+  createContext,
+  useContext,
+  useState,
+  useEffect,
+  useRef,
+} from "react";
 
 const WindowDimensionsCtx = createContext(null);
 
 const WindowDimensionsProvider = ({ children }) => {
+  const resizeInProgress = useRef(false);
+
   const [dimensions, setDimensions] = useState({
     width: typeof window !== "undefined" ? window.innerWidth : null,
     height: typeof window !== "undefined" ? window.innerHeight : null,
   });
+
   useEffect(() => {
-    const handleResize = () => {
-      setDimensions({
-        width: typeof window !== "undefined" ? window.innerWidth : null,
-        height: typeof window !== "undefined" ? window.innerHeight : null,
-      });
-    };
+    function throttledUpdateWindowSize() {
+      setTimeout(() => {
+        setDimensions({
+          width: typeof window !== "undefined" ? window.innerWidth : null,
+          height: typeof window !== "undefined" ? window.innerHeight : null,
+        });
+        resizeInProgress.current = false;
+      }, 500);
+    }
+    function handleResize() {
+      if (resizeInProgress.current === true) {
+        return;
+      }
+      resizeInProgress.current = true;
+      throttledUpdateWindowSize();
+    }
+
     window.addEventListener("resize", handleResize);
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
+    return () => window.removeEventListener("resize", handleResize);
   }, []);
+
   return (
     <WindowDimensionsCtx.Provider value={dimensions}>
       {children}
