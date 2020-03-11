b4a636c611bdcc3786e1e016b8a898de4f85d564
wildpow <element1028@gmail.com>
2020-03-10 21:19:44 -0700

    trying as background image

--- src/components/mattressList/currentSale/newCurrentSale/heading.jsx
+++ src/components/mattressList/currentSale/newCurrentSale/heading.jsx
@@ -1,18 +1,24 @@
 import React from "react";
 import styled from "styled-components";
-import test from "../../../../images/new lifestyle/test.jpg";
-import test2 from "../../../../images/new lifestyle/test2.jpg";
-import test3 from "../../../../images/new lifestyle/test3.jpg";
-import test4 from "../../../../images/new lifestyle/test4.jpg";
-import test5 from "../../../../images/new lifestyle/test5.jpg";
-import test6 from "../../../../images/new lifestyle/test6.jpg";
+import test from "../../../../images/new/test.jpg";
+import test2 from "../../../../images/new/test2.jpg";
+import test3 from "../../../../images/new/test3.jpg";
+import test4 from "../../../../images/new/test4.jpg";
+import test5 from "../../../../images/new/test5.jpg";
+import test6 from "../../../../images/new/test6.jpg";
 
 const HeaderWrapper = styled.div`
   margin-top: 10px;
-
+  background: url(${test4}) no-repeat;
+  /* background-size: cover; */
+  height: 200px;
   display: flex;
+  background-color: white;
   flex-direction: column;
-  background: white;
+  /* background: white; */
+  justify-content: flex-end;
+  align-content: center;
+  align-items: center;
   font-family: ${props => props.theme.MainFont1};
   h2 {
     margin-top: 5px;
@@ -34,8 +40,13 @@ const HeaderWrapper = styled.div`
     font-weight: 300;
     font-family: ${props => props.theme.MainFont3};
   }
-
+  .heading__content {
+    max-width: 600px;
+  }
   @media screen and (min-width: 768px) {
+    /* .heading__content {
+      margin-right: 70px;
+    } */
     flex-direction: row;
     h2 {
       border-bottom: 4px solid ${props => props.theme.mainColor2};
@@ -46,12 +57,17 @@ const HeaderWrapper = styled.div`
       font-size: 1.2rem;
     }
   }
+  @media screen and (min-width: 1200px) {
+    .heading__content {
+      margin-right: 70px;
+    }
+  }
 `;
 const Heading = ({ description, title }) => {
   return (
     <HeaderWrapper>
-      <img src={test3} alt="bla" />
-      <div>
+      {/* <img src={test3} alt="bla" /> */}
+      <div className="heading__content">
         <h2>{title}</h2>
         <p>{description}</p>
       </div>
