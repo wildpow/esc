4054ae4a79a582b8658349bd9d5cd60e1c070239
wildpow <element1028@gmail.com>
2020-03-16 11:54:24 -0700

    Stearns updated with filter/sort and new header

--- src/pages/brands/stearns/index.jsx
+++ src/pages/brands/stearns/index.jsx
@@ -3,41 +3,20 @@ import PropTypes from "prop-types";
 import { HelmetDatoCms } from "gatsby-source-datocms";
 import { graphql } from "gatsby";
 import Layout from "../../../components/layout";
-import MattressThumb from "../../../components/mattressList/mattThumbNail";
-import MattressList from "../../../components/mattressList";
 import MattListCurrentSale from "../../../components/mattressList/currentSale";
-import test3 from "../../../images/new/test3.jpg";
-import test4 from "../../../images/new/test4.jpg";
-import test5 from "../../../images/new/test5.jpg";
-import test2 from "../../../images/new/test2.jpg";
-import test from "../../../images/new/test.jpg";
 
 const Stearns = ({ data }) => {
-  const { datoCmsSeo, allDatoCmsMattress } = data;
+  const { datoCmsBrand, allDatoCmsMattress } = data;
   return (
     <Layout>
-      {/* <MattressList
-        seo={datoCmsSeo.seoMetaTags}
-        brandImgAlt="Logo of the Stearns and Foster mattress company."
-        headerText="Stearns uncover exceptional."
-        brandName="stearns"
-      >
-        {allDatoCmsMattress.nodes.map(mattress => (
-          <MattressThumb
-            key={mattress.id}
-            mattress={mattress}
-            url={`/brands/${mattress.brand.urlName}/${mattress.slug}`}
-          />
-        ))}
-      </MattressList> */}
-      <HelmetDatoCms seo={datoCmsSeo.seoMetaTags} />
+      <HelmetDatoCms seo={datoCmsBrand.seoMetaTags} />
       <MattListCurrentSale
-        headerBG={test}
+        headerBG={datoCmsBrand.headerImage.url}
         mattresses={allDatoCmsMattress.nodes}
-        title="Stearns & Foster"
-        description="Our Spring Savings Event is going on now at ESC Mattress Center in Everett, Snohomish County's favorite mattress store! Shop our best deals on mattres"
+        title={datoCmsBrand.displayName}
+        description={datoCmsBrand.tagLine}
         breadCrumbs
-        brandName="stearns"
+        brandName={datoCmsBrand.urlName}
         landing
       />
     </Layout>
@@ -52,10 +31,18 @@ export default Stearns;
 
 export const stearnsMattresses = graphql`
   query stearnsMattresses {
-    datoCmsSeo(name: { eq: "stearns mattresses" }) {
+    datoCmsBrand(urlName: { eq: "stearns" }) {
       seoMetaTags {
         ...GatsbyDatoCmsSeoMetaTags
       }
+      tagLine
+      headerImage {
+        alt
+        url
+        title
+      }
+      displayName
+      urlName
     }
     allDatoCmsMattress(
       filter: { brand: { urlName: { eq: "stearns" } } }
