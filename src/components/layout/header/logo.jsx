230bbdc5cbe62a6834f1debb471f5e426024e4e9
wildpow <element1028@gmail.com>
2020-04-15 15:29:21 -0700

    Logo component
    - Moved images to cdn.
    - Resized image for max-width use.
    - Removed unused images.
    
    SAVING: 14804 bytes

--- src/components/layout/header/logo.jsx
+++ src/components/layout/header/logo.jsx
@@ -9,49 +9,54 @@ import {
   Wrapper,
   ReadersChoice,
 } from "./logo.styles";
-import readersChoice from "../../../images/ReadersChoiceWinner300x.png";
 import { useModalContext } from "../modalContext";
 
 const Logo = ({ menuToggle }) => {
   const { modal } = useModalContext();
   return (
     <Wrapper>
-      <Link to="/blog/esc-mattress-center-wins-best-mattress-store-in-snohomish-county">
-        <ReadersChoice
-          modal={modal}
-          menuToggle={menuToggle}
-          src={readersChoice}
-          alt="ESC Mattress Center was voted SnoCo best mattress store in Snohomish County"
-        />
-      </Link>
-      <StyledLink to="/">
-        <StaticQuery
-          query={graphql`
-            query pandaLogo {
-              datoCmsFrontPage {
-                pandaLogo {
-                  url
-                  alt
-                }
+      <StaticQuery
+        query={graphql`
+          query frontImages {
+            datoCmsFrontPage {
+              sticker {
+                url
+                alt
+              }
+              pandaLogo {
+                url
+                alt
               }
             }
-          `}
-          render={data => {
-            const { pandaLogo } = data.datoCmsFrontPage;
-            return (
-              <Image
-                modal={modal}
-                src={pandaLogo.url}
-                alt={
-                  pandaLogo.alt === null
-                    ? "E.S.C Mattress Center Panda logo"
-                    : pandaLogo.alt
-                }
-              />
-            );
-          }}
-        />
-      </StyledLink>
+          }
+        `}
+        render={data => {
+          const { pandaLogo, sticker } = data.datoCmsFrontPage;
+          return (
+            <>
+              <Link to="/blog/esc-mattress-center-wins-best-mattress-store-in-snohomish-county">
+                <ReadersChoice
+                  modal={modal}
+                  menuToggle={menuToggle}
+                  src={sticker.url}
+                  alt={sticker.alt}
+                />
+              </Link>
+              <StyledLink to="/">
+                <Image
+                  modal={modal}
+                  src={pandaLogo.url}
+                  alt={
+                    pandaLogo.alt === null
+                      ? "E.S.C Mattress Center Panda logo"
+                      : pandaLogo.alt
+                  }
+                />
+              </StyledLink>
+            </>
+          );
+        }}
+      />
       <H1>
         <Span>E.S.C. </Span>
         Mattress Center
