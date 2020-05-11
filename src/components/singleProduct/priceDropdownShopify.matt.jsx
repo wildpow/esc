9853f8722d4cb1a482ab0843f5ed4e0d3b74a872
wildpow <element1028@gmail.com>
2020-05-11 15:31:52 -0700

    size select WIP

--- src/components/singleProduct/priceDropdownShopify.matt.jsx
+++ src/components/singleProduct/priceDropdownShopify.matt.jsx
@@ -3,26 +3,39 @@ import React, { useState } from "react";
 const ShopifyDropDown = ({ shopifyBase, shopifyMattress }) => {
   const [currentSize, setCurrentSize] = useState("");
   const [currentBase, setCurrentBase] = useState("");
+  const test2 = ["twin", "twinxl", "full", "queen", "king"];
+
   function createMattSelect(variants) {
-    const test = ["twin", "twinxl", "full", "queen", "king"];
-    const newVarents = [];
+    const test = { twin: 0, twinxl: 1, full: 2, queen: 3, king: 4 };
+    const newVarents = new Array(4).fill("test");
     if (variants.length === 5) return variants;
-    for (let i = 0; i < test.length; i += 1) {
-      if (variants[i].title === test[i].title) {
-        newVarents.push(variants[i]);
-      } else {
-        newVarents.push({ title: `${test[i]} (Unavalible)`, disabled: true });
-      }
+    for (let i = 0; i < variants.length; i += 1) {
+      newVarents.splice(test[variants[i].title.toLowerCase()], 1, variants[i]);
     }
     return newVarents;
   }
-  const mattressVariants = [];
+  const mattressVariants = createMattSelect(shopifyMattress.variants);
   return (
     <div>
-      {console.log("after", createMattSelect(shopifyMattress.variants))}
-      {console.log("before", shopifyMattress.variants)}
-      <select name="" id="">
-        <option value="">bla</option>
+      <small>
+        Some mattresses are only manufactured in certain sizes. See menu for
+        available sizes.
+      </small>
+      <select name="size" id="size" defaultValue="Select Size">
+        {mattressVariants.map((matt, index) => {
+          if (matt === "test") {
+            return (
+              <option value={index} key={matt.title} disabled>
+                {`${test2[index]} (Not Available)`}
+              </option>
+            );
+          }
+          return (
+            <option value={index} key={matt.title}>
+              {matt.title}
+            </option>
+          );
+        })}
       </select>
       ShopifyDropDown
     </div>
