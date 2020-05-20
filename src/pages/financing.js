aea0b21a44c3adb006dc4d3e2b3872ebcb40cc1b
wildpow <element1028@gmail.com>
2020-05-19 19:23:49 -0700

    updated financing page and Sealy mattress list

--- src/pages/financing.js
+++ src/pages/financing.js
@@ -44,18 +44,18 @@ const Financing = ({ data }) => (
         </Footer>
       </CompanyWrapper>
       <CompanyWrapper2>
-        <H3>Acima No-Credit Needed Financing</H3>
+        <H3>Acima No-Credit Needed</H3>
         <InfoWrapper>
           <Img
             src={Acima}
             alt="Acima Financing logo for low or no credit financing options"
           />
           <P>
-            Even if you have low or no credit we partner with Acima Financing to
-            extend financing and help you purchase the bed of your dreams. With
-            the option to pay off your account in 90-day, or take time to repay
-            your account over 12 months with payments scheduled to align with
-            your payday.
+            Even if you have low or no credit we partner with Acima No Credit
+            Needed to extend Lease to Own options and help you purchase the bed
+            of your dreams. With the option to pay off your account in 90-day,
+            or take time to repay your account over 12 months with payments
+            scheduled to align with your payday.
           </P>
         </InfoWrapper>
         <div>
