7438713b4050331cc288537b306e04a4b7c4fa94
wildpow <element1028@gmail.com>
2020-07-02 15:37:19 -0700

    test

--- src/components/Hooks/index.js
+++
@@ -1,3 +0,0 @@
-export { default as useKeyboardEvent } from "./use-keyboard-event";
-export { default as useOnClickOutside } from "./use-onClick-outside";
-export { default as usePrevious } from "./use-previous";
