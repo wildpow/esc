744c30096c7f934c02bae1affb33552127564657
wildpow <element1028@gmail.com>
2020-07-02 17:47:10 -0700

    test

--- src/context/WindowSizeContext.js
+++
@@ -1,7 +0,0 @@
-import React, { useContext } from "react";
-
-const WindowSizeCtx = React.createContext();
-
-export const useWindowSize = () => useContext(WindowSizeCtx);
-
-export default WindowSizeCtx;
