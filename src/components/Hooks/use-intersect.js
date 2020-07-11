83652a8c7bec84b5a3ab9b7295c8725bf08f490d
wildpow <element1028@gmail.com>
2020-07-11 12:21:36 -0700

    check for window in hook

--- src/components/Hooks/use-intersect.js
+++ src/components/Hooks/use-intersect.js
@@ -3,14 +3,15 @@ import { useEffect, useRef, useState } from "react";
 export default ({ root = null, rootMargin, threshold = 0 }) => {
   const [entry, updateEntry] = useState({});
   const [node, setNode] = useState(null);
-
-  const observer = useRef(
-    new window.IntersectionObserver(([entry]) => updateEntry(entry), {
-      root,
-      rootMargin,
-      threshold,
-    }),
-  );
+  const intersectionObserver =
+    typeof window !== `undefined`
+      ? new window.IntersectionObserver(([entry]) => updateEntry(entry), {
+          root,
+          rootMargin,
+          threshold,
+        })
+      : null;
+  const observer = useRef(intersectionObserver);
 
   useEffect(() => {
     const { current: currentObserver } = observer;
