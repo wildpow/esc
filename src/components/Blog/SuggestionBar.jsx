53441032874a0853a4e61264cce211e370a74d44
wildpow <element1028@gmail.com>
2020-09-19 01:19:44 -0700

    refactor(blog): rewrote suggestionbar component
    - less DOM nodes,
    - less brittle CSS
    - easier to read
    - less runtime impack with fewer SC components

--- src/components/Blog/SuggestionBar.jsx
+++ src/components/Blog/SuggestionBar.jsx
@@ -2,176 +2,187 @@ import React from "react";
 import PropTypes from "prop-types";
 import { Link } from "gatsby";
 import styled from "styled-components";
-import { fonts, colors } from "../../utils/styles";
+import { fonts, colors, breakpoints, fontSize } from "../../utils/styles";
 
 const StyledLink = styled(Link)`
-  padding-right: 2px;
-  padding-left: 2px;
-  padding-bottom: 5px;
-  background: white;
-  grid-template-columns: 80px 1fr;
-  grid-template-rows: 100px 1fr;
+  display: flex;
   text-decoration: none;
-  grid-template-areas:
-    "ThumbImg Title"
-    "Description Description";
-  display: grid;
   position: relative;
-  @media (min-width: 568px) {
-    padding-right: 10px;
-    padding-left: 10px;
-    padding-bottom: 5px;
-  }
-  @media (min-width: 768px) {
-    padding-right: 20px;
-    padding-left: 20px;
-    padding-bottom: 15px;
-  }
+  flex-direction: ${({ reverse }) => reverse && "row-reverse"};
+  margin-bottom: 18px;
+  background: white;
+  align-items: center;
+
   h4 {
-    grid-area: Title;
-    align-self: center;
-    justify-self: start;
-    justify-items: start;
+    text-align: ${({ reverse }) => reverse && "right"};
     margin: 0;
     padding: 0;
-    font-size: 1rem;
-    line-height: 1.3rem;
-    font-family: ${(props) => props.theme.MainFont1};
-    color: black;
-    z-index: 10;
-    @media (min-width: 375px) {
-      font-size: 1.2rem;
-    }
-    @media (min-width: 568px) {
-      font-size: 1.25rem;
-      line-height: 1.4rem;
-    }
-    @media (min-width: 768px) {
-      font-size: 1.4rem;
-    }
+    font-family: ${fonts.sans};
+    z-index: 11;
+    color: white;
+    max-width: 200px;
+    line-height: 1em;
   }
-  span {
-    top: 73px;
+  :after {
+    transition: background-color 0.2s ease-in-out;
+    bottom: 11px;
+    left: ${({ reverse }) => !reverse && "1px"};
+    right: ${({ reverse }) => reverse && "1px"};
+    content: " ";
+    height: 50px;
+    width: 90%;
+    background: ${colors.red[900]};
     position: absolute;
-    background: ${colors.gray["900"]};
-    height: 20px;
-    width: 100%;
-    /* border-radius: 4px; */
-    opacity: 0.3;
-    grid-column: span 2;
-    transition: transform 0.25s ease-in;
   }
-  :hover {
-    h4 {
-      color: ${colors.red["900"]};
-    }
-    img {
-      transform: scale3d(1.1, 1.1, 1);
-    }
-    span {
-      background-color: ${colors.blue["900"]};
-      :after {
-        border-right-color: ${(props) =>
-          props.left ? colors.blue["900"] : ""};
-        border-left-color: ${(props) =>
-          props.right ? colors.blue["900"] : ""};
-      }
-    }
-  }
-  span:after {
-    left: ${({ right }) => (right ? "100%" : "-7%")};
-    right: ${({ left }) => (left ? "100%" : "0%")};
-    top: 50%;
+  :before {
+    transition: all 0.2s ease-in-out;
+    left: ${({ reverse }) => !reverse && "90%"};
+    right: ${({ reverse }) => reverse && "90%"};
+    top: 9px;
     border: solid transparent;
     content: " ";
     height: 0;
     width: 0;
+    z-index: 11;
     position: absolute;
     pointer-events: none;
     border-color: rgba(136, 183, 213, 0);
-    border-right-color: ${(props) => (props.left ? colors.gray["900"] : "")};
-    border-left-color: ${(props) => (props.right ? colors.gray["900"] : "")};
-    border-width: 10px;
-    margin-top: -10px;
-    margin-left: ${({ left }) => left && "1px"};
+    border-right-color: ${({ reverse }) => (reverse ? colors.red["900"] : "")};
+    border-left-color: ${({ reverse }) => (!reverse ? colors.red["900"] : "")};
+    border-width: 25px;
   }
-  @media (min-width: 768px) {
-    span:after {
-      left: ${({ right }) => (right ? "100%" : "-6%")};
-      margin-left: ${({ left }) => left && "3px"};
+  img {
+    transition: transform 0.25s ease-in-out;
+    width: 70px;
+    z-index: 10;
+
+    margin-left: 5px;
+    margin-right: 5px;
+  }
+  :hover {
+    :after {
+      background: ${colors.blue[900]};
+    }
+    :before {
+      border-right-color: ${({ reverse }) =>
+        reverse ? colors.blue["900"] : ""};
+      border-left-color: ${({ reverse }) =>
+        !reverse ? colors.blue["900"] : ""};
+    }
+    img {
+      transform: scale3d(1.1, 1.1, 1);
+    }
+  }
+  @media (min-width: ${breakpoints.xsm}) {
+    h4 {
+      max-width: 85%;
+    }
+  }
+  @media (min-width: ${breakpoints.phablet}) {
+    h4 {
+      width: 100%;
+      max-width: initial;
+    }
+    img {
+      margin-left: 15px;
+      margin-right: 15px;
+    }
+  }
+  @media (min-width: ${breakpoints.md}) {
+    width: 48%;
+    h4 {
+      width: 60%;
+      line-height: 1.25em;
+      padding-top: 4px;
+    }
+    img {
+      width: 90px;
+    }
+    :after {
+      height: 60px;
+    }
+    :before {
+      border-width: 30px;
+      top: 19px;
+    }
+  }
+  @media (min-width: ${breakpoints.lg}) {
+    h4 {
+      width: 65%;
+      font-size: ${fontSize.lg};
+    }
+  }
+  @media (min-width: ${breakpoints.xl}) {
+    h4 {
+      font-size: ${fontSize["2xl"]};
+    }
+    img {
+      width: 110px;
+    }
+    :after {
+      height: 80px;
+    }
+    :before {
+      border-width: 40px;
     }
   }
 `;
 
-const ThumbImg = styled.img`
-  grid-area: ThumbImg;
-  width: 70px;
-  transition: transform 0.25s ease-in;
-  align-self: center;
-  justify-self: center;
-  z-index: 10;
-  margin-right: 10px;
-`;
-
+const rowReverse = (n, p) => {
+  if (n && p) {
+    return "row-reverse";
+  }
+  if (n === true && p === false) {
+    return "row-reverse";
+  }
+  return "row";
+};
 const SuggestionRoot = styled.div`
   display: flex;
   justify-content: space-between;
   padding-top: 20px;
-  padding-bottom: 20px;
+  padding-bottom: 0px;
   flex-direction: column;
   margin: 0 auto;
   align-content: center;
+  overflow-x: hidden;
   @media (min-width: 768px) {
-    flex-direction: row;
-  }
-`;
-
-const PostSuggestion = styled.div`
-  display: flex;
-  align-items: center;
-  max-width: 300px;
-  margin: 0 auto;
-  font-family: ${fonts.sans};
-  @media (min-width: 768px) {
-    max-width: 370px;
+    flex-direction: ${({ isPrev, isNext }) => rowReverse(isNext, isPrev)};
   }
 `;
 
 const SuggestionBar = ({ prev, next }) => {
+  const isNext = !!next;
+  const isPrev = !!prev;
   return (
-    <SuggestionRoot>
-      <PostSuggestion>
-        {prev && (
-          <StyledLink to={`/blog/${prev.slug}`} left>
-            <ThumbImg
-              src={prev.excerptImage.url}
-              alt={
-                prev.excerptImage.alt === null
-                  ? `The blog post called ${prev.title}`
-                  : prev.excerptImage.alt
-              }
-            />
-            <h4>{prev.title}</h4>
-            <span />
-          </StyledLink>
-        )}
-      </PostSuggestion>
-      <PostSuggestion>
-        {next && (
-          <StyledLink to={`/blog/${next.slug}`} right>
-            <ThumbImg
-              src={next.excerptImage.url}
-              alt={
-                next.excerptImage.alt === null
-                  ? `The blog post called ${next.title}`
-                  : next.excerptImage.alt
-              }
-            />
-            <h4>{next.title}</h4>
-            <span />
-          </StyledLink>
-        )}
-      </PostSuggestion>
+    <SuggestionRoot isNext={isNext} isPrev={isPrev}>
+      {next && (
+        <StyledLink to={`/blog/${next.slug}`} right>
+          <img
+            src={next.excerptImage.url}
+            alt={
+              next.excerptImage.alt === null
+                ? `The blog post called ${next.title}`
+                : next.excerptImage.alt
+            }
+          />
+          <h4>{next.title}</h4>
+        </StyledLink>
+      )}
+
+      {prev && (
+        <StyledLink to={`/blog/${prev.slug}`} left reverse>
+          <img
+            src={prev.excerptImage.url}
+            alt={
+              prev.excerptImage.alt === null
+                ? `The blog post called ${prev.title}`
+                : prev.excerptImage.alt
+            }
+          />
+          <h4>{prev.title}</h4>
+        </StyledLink>
+      )}
     </SuggestionRoot>
   );
 };
