41237c71d25190e89e6929e2818f5b77102c5169
wildpow <element1028@gmail.com>
2020-09-22 12:53:32 -0700

    refactor: trying new toast

---
+++ src/components/Toast2/Toast.js
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from "react";
+import "./Toast.css";
+
+const Toast = ({ toastList, position }) => {
+  const [list, setList] = useState(toastList);
+  useEffect(() => {
+    setList(toastList);
+  }, [toastList, list]);
+  return (
+    <>
+      <div className={`notification-container ${position}`}>
+        {list.map((toast, i) => (
+          <div key={i} className={`notification toast ${position}`}>
+            <button>X</button>
+            <div className="notification-image">
+              <img src={toast.icon} alt="" />
+            </div>
+            <div>
+              <p className="notification-title">{toast.title}</p>
+              <p className="notification-message">{toast.description}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
+export default Toast;
