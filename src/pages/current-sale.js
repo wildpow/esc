46f6eb519fc8d135e4e1ff0b05ea5d540d4ceaca
wildpow <element1028@gmail.com>
2021-01-14 15:41:33 -0800

    feat(current-sale): now works like brands route with breadcrumbs on sm viewports

--- src/pages/current-sale.js
+++ src/pages/current-sale.js
@@ -1,9 +1,18 @@
 import React from "react";
+import styled from "styled-components";
 import PropTypes from "prop-types";
 import { graphql } from "gatsby";
 import { HelmetDatoCms } from "gatsby-source-datocms";
 import Layout from "../components/Layout";
 import MattressList from "../components/MattressList";
+import BreadCrumbs, { BreadWrapper } from "../components/BreadCrumbs";
+
+const BrandsRoot = styled.div`
+  @media (min-width: 1022px) {
+    padding-top: 20px;
+    padding-bottom: 20px;
+  }
+`;
 
 const Sale = ({ data }) => {
   const { allDatoCmsNewMattress, datoCmsFrontPage } = data;
@@ -15,13 +24,21 @@ const Sale = ({ data }) => {
   return (
     <Layout>
       <HelmetDatoCms seo={datoCmsFrontPage.currentSaleSeoLink.seoMetaTags} />
-      <MattressList
-        headerBG={datoCmsFrontPage.currentSaleHeaderLink.bgImg.url}
-        mattresses={allSaleMattresses}
-        title={datoCmsFrontPage.currentSaleHeaderLink.title}
-        description={datoCmsFrontPage.currentSaleHeaderLink.tagLine}
-        button={{ label: "Shop all Mattresses", url: "/brands/list" }}
-      />
+      <BreadWrapper hidenLarge>
+        <BreadCrumbs here="Current Sale" />
+      </BreadWrapper>
+      <BrandsRoot>
+        <MattressList
+          headerBG={datoCmsFrontPage.currentSaleHeaderLink.bgImg.url}
+          mattresses={allSaleMattresses}
+          title={datoCmsFrontPage.currentSaleHeaderLink.title}
+          description={datoCmsFrontPage.currentSaleHeaderLink.tagLine}
+          button={{ label: "Shop all Mattresses", url: "/brands/list" }}
+        />
+      </BrandsRoot>
+      <BreadWrapper hidenLarge Bottom>
+        <BreadCrumbs here="Current Sale" />
+      </BreadWrapper>
     </Layout>
   );
 };
