9dbc23e03afc38e1421e30d2718779adbe378a4f
wildpow <element1028@gmail.com>
2021-02-24 15:31:47 -0800

    refactor: rewrote cart counter

--- src/utils/keyframes.js
+++ src/utils/keyframes.js
@@ -14,12 +14,26 @@ export const iconEntry = keyframes`
 
 export const numberEntry = keyframes`
   0%{
-    transform: scale(0)
+    transform: scale(0);
   }
   90% {
     transform: scale(0.7);
   }
   100% {
     transform: scale(0.6);
+  }
+`;
+
+export const numberEntry2 = keyframes`
+  0%{
+    transform: scale(0.2);
+
+  }
+  90% {
+    transform: scale(1.2);
+  }
+  100% {
+    transform: scale(0.75);
+
   }
 `;
