70e9e0cc47d7c35a87d657f9510d52823b57dbba
wildpow <element1028@gmail.com>
2021-03-09 16:14:21 -0800

    refactor: foundation

--- src/components/SingleProduct/FoundationCompat.jsx
+++ src/components/SingleProduct/FoundationCompat.jsx
@@ -5,13 +5,37 @@ import useFoundationImages from "./fCompatImgHook";
 
 const FoundationRoot = styled.div`
   .lgWrapper {
-    display: flex;
-    width: 100%;
+    display: grid;
+    grid-template-areas: "one two three four";
+    /* grid-template-rows: 50px 1fr 30px; */
+    grid-template-columns: repeat(4, minMax(150px, 250px));
+    justify-content: space-around;
+    gap: 25px;
   }
-  .smWrapper {
+  @media (max-width: 1200px) {
+    .lgWrapper {
+      grid-template-columns: repeat(2, minMax(150px, 250px));
+      grid-template-areas:
+        "one two"
+        "three four";
+    }
+  }
+  /* .smWrapper {
     display: flex;
     width: 100%;
     justify-content: space-evenly;
+  } */
+  .one {
+    grid-area: one;
+  }
+  .two {
+    grid-area: two;
+  }
+  .three {
+    grid-area: three;
+  }
+  .four {
+    grid-area: four;
   }
   .imgWrapper {
     max-width: 250px;
@@ -35,27 +59,25 @@ export default function FoundationCompat(props) {
     <FoundationRoot>
       <h4>Works With All Bed Types</h4>
       <div className="lgWrapper">
-        <div className="smWrapper">
-          <div className="imgWrapper">
-            <Img fluid={box.childImageSharp.fluid} />
-            <p>Box Spring</p>
-          </div>
-          <div className="imgWrapper">
-            <Img fluid={frame.childImageSharp.fluid} />
-            <p>Traditional Frames</p>
-          </div>
+        <div className="imgWrapper one">
+          <Img fluid={box.childImageSharp.fluid} />
+          <p>Box Spring</p>
         </div>
-        <div className="smWrapper">
-          <div className="imgWrapper">
-            <Img fluid={adjBase.childImageSharp.fluid} />
-            <p>Ajustable Bases</p>
-          </div>
-          <div className="imgWrapper">
-            <Img fluid={platform.childImageSharp.fluid} />
-            <p>Platform</p>
-          </div>
+        <div className="imgWrapper two">
+          <Img fluid={frame.childImageSharp.fluid} />
+          <p>Traditional Frames</p>
+        </div>
+
+        <div className="imgWrapper three">
+          <Img fluid={adjBase.childImageSharp.fluid} />
+          <p>Ajustable Bases</p>
+        </div>
+        <div className="imgWrapper four">
+          <Img fluid={platform.childImageSharp.fluid} />
+          <p>Platform</p>
         </div>
       </div>
+
       <p>
         Your mattress purchase is fully compatible with all modern support
         systems, including adjustable base, platform bed, box
