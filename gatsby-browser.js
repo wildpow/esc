f773399a7d2c8177660ba45cd8b35e4ebf159ea0
wildpow <element1028@gmail.com>
2021-04-13 16:22:13 -0700

    feat: moved from deprecated typeface fonts to fontsource fonts

---
+++ gatsby-browser.js
@@ -0,0 +1,6 @@
+import "@fontsource/roboto";
+import "@fontsource/roboto/300.css"; // footer
+import "@fontsource/roboto/500.css"; // footer
+import "@fontsource/roboto/900.css"; // logo
+import "@fontsource/roboto/900-italic.css"; // logo
+import "@fontsource/roboto-slab";
