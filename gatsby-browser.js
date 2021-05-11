e084a6cfcb19bbb870c72ad695d940b462c1e9c4
wildpow <element1028@gmail.com>
2021-05-11 16:40:54 -0700

    chore: update deps

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,5 +1,6 @@
 import "@fontsource/roboto";
 import "@fontsource/roboto-slab";
+import "@fontsource/roboto-slab/300.css"; // post thumbnail
 import "@fontsource/roboto/700.css"; // productList__header
 import "@fontsource/roboto/900.css"; // logo
 import "@fontsource/roboto/900-italic.css"; // logo
