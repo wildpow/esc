b419f36333bdb4dd971665168cdd8e8704fa294e
wildpow <element1028@gmail.com>
2021-05-11 17:02:49 -0700

    refactor: adjustable

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,8 @@
 import "@fontsource/roboto";
 import "@fontsource/roboto-slab";
 import "@fontsource/roboto-slab/300.css"; // post thumbnail
+import "@fontsource/roboto-slab/700.css"; // ajustable thumb
+
 import "@fontsource/roboto/700.css"; // productList__header
 import "@fontsource/roboto/900.css"; // logo
 import "@fontsource/roboto/900-italic.css"; // logo
--- src/pages/adjustable.js
+++ src/pages/adjustable.js
@@ -7,9 +7,12 @@ import PropTypes from "prop-types";
 import BreadCrumbs from "../components/BreadCrumbs";
 import Layout from "../components/Layout";
 import FadeIn from "../keyframes/fadeIn.styled";
+import fontSize from "../Old/fontSizes";
 
 const adjustableRoot = css`
   animation: ${FadeIn} 0.35s linear normal;
+  animation-duration: 0.5s;
+  animation-fill-mode: both;
   display: flex;
   flex-direction: column;
   margin-left: 0px;
@@ -102,6 +105,7 @@ const adjustableFeatures = css`
     }
     ul {
       margin-top: 10px;
+      margin-left: 21px;
     }
     li {
       padding-bottom: 2px;
@@ -128,6 +132,168 @@ const adjustableFeatures = css`
     }
   }
 `;
+const FeaturePriceWrapper = css`
+  display: flex;
+  justify-content: space-between;
+  flex-direction: column;
+  width: 50%;
+  display: none;
+
+  @media (min-width: 568px) {
+    display: flex;
+  }
+`;
+const priceRange = css`
+  color: ${theme`colors.gray[700]`};
+  font-family: ${theme`fontFamily.sans`};
+  font-size: ${fontSize.xl};
+  font-weight: 700;
+  padding-bottom: ${theme`spacing[5]`};
+  @media (min-width: 768px) {
+    letter-spacing: 0.05rem;
+  }
+`;
+const PriceRangeLg = css`
+  padding-left: 20px;
+  align-self: flex-start;
+  @media (min-width: 768px) {
+    align-self: flex-end;
+    padding-right: 20px;
+  }
+  @media (min-width: 1024px) {
+    font-size: ${fontSize["3xl"]};
+  }
+`;
+const PriceRangeSm = css`
+  padding-right: 20px;
+  align-self: flex-end;
+  @media (min-width: 568px) {
+    display: none;
+  }
+`;
+const ImageWrapper = css`
+  width: 100%;
+  justify-self: center;
+  align-self: center;
+  @media (min-width: 568px) {
+    width: 50%;
+  }
+`;
+
+const BannerWrapper = css`
+  position: relative;
+  display: flex;
+`;
+const Banner = css`
+  font-family: ${theme`fontFamily.sans`};
+  font-weight: 400;
+  text-align: center;
+  z-index: 10;
+  background-color: ${theme`colors.brandRed`};
+  color: white;
+  position: absolute;
+  font-size: 0.8rem;
+  padding-top: 5px;
+  padding-bottom: 5px;
+  width: 60%;
+  margin-left: 0px;
+  letter-spacing: 0.105rem;
+  top: 6%;
+  &:after {
+    content: " ";
+    position: absolute;
+    display: block;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+    z-index: -1;
+    background: ${theme`colors.brandRed`};
+    transform-origin: bottom left;
+    transform: skew(-21deg, 0deg);
+  }
+  @media (min-width: 375px) and (orientation: portrait) {
+    padding-top: 5px;
+    padding-bottom: 5px;
+    font-size: 0.9rem;
+  }
+  @media (max-width: 812px) and (orientation: landscape) {
+    font-size: 1.2rem;
+    width: 85%;
+    padding-top: 5px;
+    padding-bottom: 5px;
+  }
+  @media (min-width: 768px) and (orientation: portrait) {
+    font-size: 1rem;
+    padding-top: 5px;
+    padding-bottom: 5px;
+    width: 80%;
+  }
+  @media (min-width: 813px) and (orientation: landscape) {
+    font-size: 1.1rem;
+    padding-top: 5px;
+    padding-bottom: 5px;
+    width: 80%;
+  }
+  @media (min-width: 1024px) and (orientation: landscape) {
+    font-size: 1.3rem;
+    padding-top: 5px;
+    padding-bottom: 5px;
+    width: 80%;
+  }
+  @media (min-width: 1024px) and (orientation: portrait) {
+    font-size: 1.4rem;
+    padding-top: 5px;
+    padding-bottom: 5px;
+    width: 80%;
+  }
+  @media (min-width: 1300px) {
+    width: 70%;
+    font-size: 1.4rem;
+  }
+`;
+const ImageContainer = css`
+  color: white;
+  width: 240px;
+  height: 240px;
+  margin: 0 auto;
+  @media (min-width: 991px) {
+    width: 350px;
+    height: 350px;
+    align-self: center;
+  }
+`;
+const H3 = css`
+  font-weight: 700;
+  font-family: ${theme`fontFamily`};
+  background-color: ${theme`colors.brandBlue`};
+  color: ${theme`colors.blueGray["50"]`};
+  margin-top: 0;
+  text-align: center;
+  margin-bottom: 0;
+  font-size: 1rem;
+  padding: 10px 5px 10px 5px;
+  letter-spacing: 0.12rem;
+  @media (min-width: 560px) {
+    font-size: 1.4rem;
+    padding: 10px 30px 10px 30px;
+  }
+  @media (min-width: 692px) {
+    word-spacing: 0.18rem;
+    letter-spacing: 0.17rem;
+  }
+  @media (min-width: 731px) {
+    font-size: 1.5rem;
+  }
+  @media (min-width: 1024px) {
+    letter-spacing: 0.19rem;
+    font-size: 1.75rem;
+  }
+  @media (min-width: 1300px) {
+    font-size: 1.8rem;
+    text-align: left;
+  }
+`;
 const Adjustables = ({ data }) => {
   const { allDatoCmsProduct } = data;
   const sortedBases = allDatoCmsProduct.nodes.sort(
@@ -146,14 +312,14 @@ const Adjustables = ({ data }) => {
             key={base.id}
             className="adjustable__card"
           >
-            <h3>{base.title}</h3>
+            <h3 className={H3}>{base.title}</h3>
             <div className="adjustable__card__content">
-              <div className="ImageWrapper">
-                <div className="BannerWrapper">
+              <div className={ImageWrapper}>
+                <div className={BannerWrapper}>
                   {base.saleBanner.length > 3 && (
-                    <div className="Banner">{base.saleBanner}</div>
+                    <div className={Banner}>{base.saleBanner}</div>
                   )}
-                  <div className="ImageContainer">
+                  <div className={ImageContainer}>
                     <GatsbyImage
                       image={getImage(base.threeImageBlock[0].coverImage)}
                       alt={base.threeImageBlock[0].coverImage.alt}
@@ -161,7 +327,7 @@ const Adjustables = ({ data }) => {
                   </div>
                 </div>
               </div>
-              <div className="FeaturePriceWrapper">
+              <div className={FeaturePriceWrapper}>
                 <div className={adjustableFeatures}>
                   <h3>Features</h3>
                   <ul>
@@ -170,7 +336,7 @@ const Adjustables = ({ data }) => {
                     ))}
                   </ul>
                 </div>
-                <div className="PriceRangeLg">
+                <div className={`${priceRange} ${PriceRangeLg} `}>
                   {`$${Math.trunc(
                     base.shopifyInfo[0].priceRange.minVariantPrice.amount
                   )}
@@ -180,7 +346,7 @@ const Adjustables = ({ data }) => {
                 </div>
               </div>
             </div>
-            <div className="PriceRangeSm">
+            <div className={`${priceRange} ${PriceRangeSm} `}>
               {`$${Math.trunc(
                 base.shopifyInfo[0].priceRange.minVariantPrice.amount
               )}
