8ddf2c7dae7761687d4cec3be0df052aa74f2735
wildpow <element1028@gmail.com>
2021-05-12 19:36:05 -0700

    refactor: working on current sale page

---
+++ src/components/ProductListing/MattressList/MattressList.jsx
@@ -0,0 +1,99 @@
+import { useReducer } from "react";
+import PropTypes from "prop-types";
+import Header from "../ListingHeader";
+import FilterSortPanel from "./FilterSortPanelMatt";
+import filterSortReducer from "./FilterSortReducer";
+import BreadCrumbs from "../BreadCrumbs";
+import {
+  NewBread,
+  MattListWrapper,
+} from "../shared/ProductList/ProductList.styled";
+import ProductThumbnail from "../shared/ProductList/ProductThumbnail";
+
+const MattressList = ({
+  mattresses,
+  title,
+  description,
+  breadCrumbs,
+  brandName,
+  headerBG,
+  button,
+}) => {
+  const initalState = {
+    mattresses,
+    beforeFilter: mattresses,
+    checkBoxs: [
+      { id: 0, value: "Extra Firm", firmness: 1 },
+      { id: 1, value: "Firm", firmness: 2 },
+      { id: 2, value: "Medium", firmness: 3 },
+      { id: 3, value: "Plush", firmness: 4 },
+      { id: 4, value: "Ultra Plush", firmness: 5 },
+    ],
+    firmnessNums: [],
+  };
+  const [state, dispatch] = useReducer(filterSortReducer, initalState);
+  return (
+    <MattListWrapper breadCrumbs={breadCrumbs}>
+      {breadCrumbs && (
+        <NewBread>
+          <BreadCrumbs next="Brands" here={brandName} />
+        </NewBread>
+      )}
+      <Header
+        title={title}
+        description={description}
+        headerBG={headerBG}
+        brandName={brandName}
+        button={button}
+      />
+
+      <div className="mattList__flex">
+        <FilterSortPanel
+          dispatch={dispatch}
+          checkBoxs={state.checkBoxs}
+          length={state.mattresses.length}
+        />
+        {state.mattresses.length > 0 ? (
+          <div className="mattList__grid">
+            {state.mattresses.map((mattress) => (
+              <ProductThumbnail
+                mattress
+                key={mattress.id}
+                product={mattress}
+                url={`/brands/${mattress.brand.urlName}/${mattress.slug}`}
+              />
+            ))}
+          </div>
+        ) : (
+          <div className="noFilter">
+            <h3>No products match these filters</h3>
+          </div>
+        )}
+      </div>
+      {breadCrumbs && (
+        <NewBread Brands Bottom>
+          <BreadCrumbs next="Brands" here={brandName} />
+        </NewBread>
+      )}
+    </MattListWrapper>
+  );
+};
+
+MattressList.propTypes = {
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  mattresses: PropTypes.instanceOf(Object).isRequired,
+  breadCrumbs: PropTypes.bool,
+  brandName: PropTypes.string,
+  button: PropTypes.shape({ label: PropTypes.string, url: PropTypes.string }),
+  headerBG: PropTypes.string,
+};
+
+MattressList.defaultProps = {
+  breadCrumbs: false,
+  brandName: "Nothing",
+  headerBG: "",
+  button: null,
+};
+
+export default MattressList;
---
+++ src/pages/current-sale.js
@@ -0,0 +1,89 @@
+import styled from "styled-components";
+import PropTypes from "prop-types";
+import { graphql } from "gatsby";
+import { HelmetDatoCms } from "gatsby-source-datocms";
+import Layout from "../components/Layout";
+import MattressList from "../components/ProductListing/MattressList";
+import BreadCrumbs, { BreadWrapper } from "../components/BreadCrumbs";
+
+const BrandsRoot = styled.div`
+  @media (min-width: 1022px) {
+    padding-top: 20px;
+    padding-bottom: 20px;
+  }
+`;
+
+const Sale = ({ data }) => {
+  const { allDatoCmsNewMattress, datoCmsFrontPage } = data;
+  const sortedMatt = allDatoCmsNewMattress.nodes.sort(
+    (a, b) =>
+      Number(a.shopifyInfo[0].priceRange.minVariantPrice.amount) -
+      Number(b.shopifyInfo[0].priceRange.minVariantPrice.amount)
+  );
+  return (
+    <Layout>
+      <HelmetDatoCms seo={datoCmsFrontPage.currentSaleSeoLink.seoMetaTags} />
+      <BreadWrapper hidenLarge>
+        <BreadCrumbs here="Current Sale" />
+      </BreadWrapper>
+      <BrandsRoot>
+        <MattressList
+          headerBG={datoCmsFrontPage.currentSaleHeaderLink.bgImg.url}
+          mattresses={sortedMatt}
+          title={datoCmsFrontPage.currentSaleHeaderLink.title}
+          description={datoCmsFrontPage.currentSaleHeaderLink.tagLine}
+          button={{ label: "Shop all Mattresses", url: "/brands/list" }}
+        />
+      </BrandsRoot>
+      <BreadWrapper hidenLarge Bottom>
+        <BreadCrumbs here="Current Sale" />
+      </BreadWrapper>
+    </Layout>
+  );
+};
+
+Sale.propTypes = {
+  data: PropTypes.instanceOf(Object).isRequired,
+};
+
+export const currentSaleQuery = graphql`
+  query currentSaleQuery {
+    allDatoCmsNewMattress(
+      filter: {
+        meta: { status: { eq: "published" } }
+        saleBanner: { ne: "NEW MODEL", regex: "/[a-z]/gi" }
+      }
+    ) {
+      nodes {
+        ...newMattressList
+      }
+    }
+
+    datoCmsFrontPage {
+      currentSaleSeoLink {
+        seoMetaTags {
+          ...GatsbyDatoCmsSeoMetaTags
+        }
+      }
+      currentSaleHeaderLink {
+        title
+        tagLine
+        bgImg {
+          url
+          alt
+          title
+        }
+      }
+    }
+  }
+`;
+
+export default Sale;
+
+// datoCmsCurrentSale {
+//   title
+//   description
+//   seoMetaTags {
+//     ...GatsbyDatoCmsSeoMetaTags
+//   }
+// }
