98df43dd47430ed88c52b716b516820f1c7d7ee2
wildpow <element1028@gmail.com>
2021-05-26 19:12:37 -0700

    refactor: added back gatsby-ssr file to fix build errors

---
+++ gatsby-ssr.js
@@ -0,0 +1,10 @@
+/* eslint-disable import/prefer-default-export */
+
+import WindowSizeProvider from "./src/contexts/WindowSize.ctx";
+import StoreProvider from "./src/contexts/Store.ctx";
+
+export const wrapRootElement = ({ element }) => (
+  <WindowSizeProvider>
+    <StoreProvider>{element}</StoreProvider>
+  </WindowSizeProvider>
+);
