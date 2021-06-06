ff46df3f407522276c92d2b6ddbcf96e09530679
wildpow <element1028@gmail.com>
2021-06-06 09:13:33 -0700

    refactor(x-chair): data into component

--- src/templates/x-chair.jsx
+++ src/templates/x-chair.jsx
@@ -1,7 +1,7 @@
 import { graphql } from "gatsby";
 import Layout from "../components/Layout";
 
-export default function XChair(data) {
+export default function XChair({ data }) {
   const test = "X-Chair";
   return (
     <Layout>
