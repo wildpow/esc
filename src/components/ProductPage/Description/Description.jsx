83a3c360fc54eafd9b1a90b1a9703c002e6ef8b7
wildpow <element1028@gmail.com>
2021-07-07 18:44:56 -0700

    fix(description - productpage): proptype profile should be string

--- src/components/ProductPage/Description/Description.jsx
+++ src/components/ProductPage/Description/Description.jsx
@@ -139,7 +139,7 @@ DescriptionSection.defaultProps = {
 DescriptionSection.propTypes = {
   mattress: PropTypes.bool,
   warranty: PropTypes.string,
-  profile: PropTypes.number,
+  profile: PropTypes.string,
   description: PropTypes.string.isRequired,
   featureList: PropTypes.instanceOf(Object),
   width: PropTypes.number.isRequired,
