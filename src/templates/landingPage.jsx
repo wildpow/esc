1896e6073556ccb29142fa70e49d6e951e30a908
wildpow <element1028@gmail.com>
2021-10-13 16:50:25 -0700

    feat: brand landing pages now generated on build instead of manually

---
+++ src/templates/landingPage.jsx
@@ -0,0 +1,43 @@
+import { graphql } from "gatsby";
+import PropTypes from "prop-types";
+import Layout from "../components/Layout";
+import Landing from "../components/Landing";
+import BreadCrumbs, { BreadWrapper } from "../components/BreadCrumbs";
+
+const LandingPage = ({ data }) => (
+  <Layout bgWhite>
+    <BreadWrapper Brands>
+      <BreadCrumbs
+        next="Brands"
+        here2={data.datoCmsBrand.displayName}
+        here="Landing"
+      />
+    </BreadWrapper>
+    <Landing
+      data={data.datoCmsBrand.landingPage}
+      buttonName="Beautyrest"
+      buttonURL="/brands/beautyrest"
+    />
+    <BreadWrapper Brands>
+      <BreadCrumbs next="Brands" here={data.datoCmsBrand.displayName} />
+    </BreadWrapper>
+  </Layout>
+);
+
+LandingPage.propTypes = {
+  data: PropTypes.instanceOf(Object).isRequired,
+};
+
+export const landingTemplete = graphql`
+  query landingTemplete($urlName: String!) {
+    datoCmsBrand(urlName: { eq: $urlName }) {
+      urlName
+      displayName
+      landingPage {
+        ...landing
+      }
+    }
+  }
+`;
+
+export default LandingPage;
