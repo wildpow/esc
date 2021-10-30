ac95de99c5bbb54d7d1313d4a05b7dca9f282748
wildpow <element1028@gmail.com>
2021-10-29 21:30:57 -0700

    refactor(grid home page): working on why shop icons

---
+++ src/components/Grid/NewBird.jsx
@@ -0,0 +1,208 @@
+import { OutboundLink } from "gatsby-plugin-google-gtag";
+import { StaticQuery, graphql } from "gatsby";
+import styled from "@emotion/styled";
+import { colors, fonts, fontSize, rounded } from "../../styles/theme.styled";
+
+const BrideyeWrapper = styled(OutboundLink)`
+  /* 
+  width: 100%;
+ 
+  max-width: 187px;
+  max-height: 130px;
+  padding: 5px 5px;
+ */
+  text-decoration: none;
+  display: grid;
+  justify-content: space-between;
+  background-color: ${colors.gray[100]};
+  border-radius: ${rounded.md};
+  border: 1px solid ${colors.gray[400]};
+  font-family: ${fonts.sans};
+  color: ${colors.blue["900"]};
+  font-weight: 500;
+  flex: 1;
+  /* max-width: 173px; */
+  grid-template-columns: 85px 85px;
+  /* grid-template-rows: 1fr 1fr; */
+  /* max-height: 137px; */
+  /* padding: 5px 5px; */
+  .bird__cert {
+    max-height: 80px;
+    width: 100%;
+    max-width: 60px;
+    grid-column: 2/2;
+    grid-row: 1/1;
+    justify-self: center;
+  }
+  .bird__content {
+    grid-column: 1/1;
+    grid-row: 1/1;
+    padding: 0 10px;
+    font-size: ${fontSize.lg};
+    justify-self: center;
+    align-self: center;
+    p {
+      margin: 0;
+    }
+  }
+  .bird__starWrapper {
+    max-height: 100px;
+    grid-row: 2/2;
+    grid-column: 1/3;
+    display: flex;
+    padding: 10px 5px 10px 5px;
+    justify-content: space-around;
+  }
+  .bird__star {
+    max-width: 25px;
+  }
+`;
+
+const CertReview = styled.div`
+  /* transition: all 0.25s ease-in;
+  &:hover {
+    transform: scale3d(1.05, 1.05, 1);
+  }
+  :focus {
+    box-shadow: 0 0 0 3px ${colors.blue["300"]};
+    outline: 0;
+    transition: box-shadow 0.15s ease-in-out;
+  }
+  width: 100%;
+  max-width: 187px;
+  max-height: 130px;
+  flex: 1; */
+  font-family: ${fonts.sans};
+  /* background-color: ${colors.gray["100"]}; */
+  display: flex;
+  color: ${colors.blue["900"]};
+  /* border-radius: 5px; */
+  /* justify-content: space-between; */
+  div h4 {
+    font-weight: 500;
+    margin: 0;
+    padding: 0;
+    font-size: 1rem;
+  }
+  @media (min-width: 768px) {
+    line-height: 1.1em;
+    letter-spacing: 0.14em;
+    div h4 {
+      font-size: 1.2rem;
+    }
+  }
+  @media print {
+    color: black;
+    box-shadow: none;
+    text-shadow: none;
+  }
+`;
+
+const Words = styled.div`
+  padding: 7px 0 0 9px;
+  display: flex;
+  flex-direction: column;
+  @media (min-width: 768px) {
+    padding-top: 10px;
+    padding-left: 10px;
+  }
+`;
+const Cert = styled.img`
+  margin-top: -3px;
+  /* margin-right: 7px; */
+  width: 45px;
+  justify-self: start;
+  height: 70px;
+  transform: scale(1);
+`;
+
+const Rating = styled.div`
+  padding-top: 10px;
+  display: flex;
+  img {
+    width: 25px;
+    height: 25px;
+  }
+  @media (min-width: 768px) {
+    padding-top: 5px;
+  }
+`;
+
+const AvgContainer = styled.div`
+  margin-top: 2px;
+  font-size: 0.8em;
+  margin-left: 4px;
+  font-family: ${fonts.sans};
+  @media (min-width: 812px) {
+    margin-top: 0px;
+    font-size: 0.9em;
+  }
+`;
+
+const Bird = () => {
+  const starsArr = [];
+  let avgRating;
+  let reviewCount;
+  return (
+    <StaticQuery
+      query={graphql`
+        query newBirdey {
+          allWidget {
+            nodes {
+              reviewCount
+              avgRating
+            }
+          }
+        }
+      `}
+      render={(data) => {
+        if (data.allWidget.nodes[0].avgRating) {
+          avgRating = data.allWidget.nodes[0].avgRating;
+          reviewCount = data.allWidget.nodes[0].reviewCount;
+        } else {
+          avgRating = data.allWidget.nodes[1].avgRating;
+          reviewCount = data.allWidget.nodes[1].reviewCount;
+        }
+        for (let i = 0; i < avgRating; i += 1) {
+          starsArr.push(
+            <img
+              src="/star.png"
+              alt="start for rating"
+              key={i + 200}
+              loading="lazy"
+              className="bird__star"
+            />
+          );
+        }
+        return (
+          <>
+            <BrideyeWrapper
+              href="https://birdeye.com/esc-mattress-center-154743411347922"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <div className="bird__content">
+                <p>
+                  {reviewCount}
+                  <br />
+                  Certified
+                  <br />
+                  Reviews
+                </p>
+              </div>
+              <div className="bird__starWrapper">{starsArr}</div>
+              <img
+                alt="BirdEye certified seal"
+                src="/badge.png"
+                className="bird__cert"
+                loading="lazy"
+              />
+            </BrideyeWrapper>
+          </>
+        );
+      }}
+    />
+  );
+};
+
+export default Bird;
--- src/pages/grid_test2.js
+++ src/pages/grid_test2.js
@@ -4,11 +4,15 @@ import { StaticImage } from "gatsby-plugin-image";
 import Layout from "../components/Layout";
 import {
   boxShadow,
+  boxShadowHover,
   colors,
   fonts,
   fontSize,
+  radius,
+  rounded,
   spacing,
 } from "../styles/theme.styled";
+import Bird from "../components/Grid/NewBird";
 
 const HomeRoot = styled.div`
   position: relative;
@@ -32,7 +36,7 @@ const HomeRoot = styled.div`
       grid-row: 3/4;
       grid-column: 1/13;
       position: relative;
-      bottom: 16px;
+      bottom: 32px;
       z-index: 10;
     }
     grid-row: 4/7;
@@ -43,8 +47,48 @@ const HomeRoot = styled.div`
     grid-template-columns: repeat(12, 1fr);
     grid-gap: 16px;
     display: grid;
+
+    .home__why__icons {
+      grid-column: 6/13;
+      grid-row: 2/5;
+      position: relative;
+      bottom: 26px;
+      z-index: 10;
+      align-self: flex-end;
+      z-index: 100;
+      /* bottom: 40px; */
+      position: relative;
+      z-index: 100;
+      /* display: grid;
+      grid-template-columns: repeat(7, 1fr); */
+      justify-content: space-evenly;
+      align-items: center;
+      display: flex;
+      .home__why__icons__bbb {
+        /* grid-column: 1/3; */
+        display: flex;
+        justify-content: center;
+        background-color: ${colors.gray[100]};
+        border-radius: ${rounded.md};
+        border: 1px solid ${colors.gray[400]};
+        padding: 5px;
+        max-width: 175px;
+      }
+      .home__why__icons__reviews {
+        /* grid-column: 3/5; */
+        display: flex;
+        justify-content: center;
+      }
+      .home__why__icons__readers {
+        /* grid-column: 5/7; */
+        display: flex;
+        justify-content: center;
+      }
+    }
+
     .home__why__info {
       grid-gap: 16px;
+      max-height: 506px;
       ::after {
         content: "";
         width: 100%;
@@ -61,7 +105,9 @@ const HomeRoot = styled.div`
       /* height: 100%; */
       /* max-height: 400px; */
       background-color: white;
-      box-shadow: ${boxShadow.md};
+      /* box-shadow: ${boxShadow.md}; */
+      box-shadow: rgba(46, 41, 51, 0.08) 0px 1px 2px,
+        rgba(71, 63, 79, 0.08) 0px 2px 4px;
       z-index: 10;
       grid-template-columns: repeat(7, 1fr);
       display: grid;
@@ -100,6 +146,7 @@ const HomeRoot = styled.div`
     height: 100%;
     flex: 1;
     display: flex;
+    ${boxShadowHover}
   }
   .home__buy {
     grid-column: 1/13;
@@ -109,9 +156,11 @@ const HomeRoot = styled.div`
     grid-gap: 16px;
     display: grid;
     .home__buy__item {
+      ${boxShadowHover}
       grid-template-columns: repeat(3, 1fr);
       grid-gap: 16px;
       display: grid;
+
       /* max-height: 200px; */
       .home__buy__text {
         padding: 15px 0;
@@ -297,6 +346,37 @@ export default function GridTest2() {
               customer-friendly 90 day comfort guarantee we've got your back in
               case you're not 100% satisfied with your purchase.
             </p>
+          </div>
+          <div className="home__why__icons">
+            <div className="home__why__icons__bbb">
+              <a
+                href="https://www.bbb.org/us/wa/everett/profile/mattress/esc-mattress-center-1296-1000056497/#sealclick"
+                target="_blank"
+                rel="nofollow noreferrer"
+                style={{ width: "100%" }}
+              >
+                <img
+                  // width={187}
+                  // height={130}
+                  loading="lazy"
+                  src="https://seal-hawaii.bbb.org/seals/darkgray-seal-187-130-bbb-1000056497.png"
+                  style={{ border: 0, width: "100%" }}
+                  alt="ESC Mattress Center BBB Business Review"
+                />
+              </a>
+            </div>
+            <div className="home__why__icons__reviews">
+              <Bird />
+            </div>
+            <div className="home__why__icons__readers">
+              <StaticImage
+                src="../images/gridTest/readerschoicewinner2019.png"
+                width={160}
+                height={160}
+                layout="constrained"
+                alt="test"
+              />
+            </div>
           </div>
         </div>
       </HomeRoot>
